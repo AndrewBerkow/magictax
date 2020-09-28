018238a0f5055bae1b6e6f122b334b26cbba3982
Andrew Berkow <andrewberkow@andrews-mbp.attlocal.net>
2020-09-28 17:57:03 -0500

    Add portal to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { PortalComponent } from './portal/portal.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/portal', pathMatch: 'full' },
+  { path: 'portal', component: PortalComponent},
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
