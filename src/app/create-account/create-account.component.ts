ec8764d69ff9ac2272e3f500cf7dd6f3dcfe49db
Andrew Berkow <andrewberkow@andrews-mbp.attlocal.net>
2020-10-17 13:45:37 -0500

    Create account

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { SessionService } from '../session.service';
+import {Form, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  emailFormGroup: FormGroup;
+  passwordFromGroup: FormGroup;
+
+  constructor(
+    private router: Router,
+    private sessionService: SessionService,
+    private _formBuilder: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    //if authed redirect away from this page
+    if(this.sessionService.authed){
+      this.sessionService.checkAuthAndRedirect();
+    }
+
+    this.emailFormGroup = this._formBuilder.group({
+      emailCtrl: ['', Validators.required, Validators.email]
+    });
+
+    this.passwordFromGroup = this._formBuilder.group({
+      passwordCtrl: ['', Validators.required]
+    });
   }
+
+  createAccount(): void {
+    //@todo - probably should do something here.
+    this.sessionService.authed = true;
+    this.sessionService.checkAuthAndRedirect();
+  }
+
 
 }
